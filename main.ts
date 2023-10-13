0b55b8517117eef90c12ba749c23e4820660074a
4-R-Forum <8607712+4-R-Forum@users.noreply.github.com>
2023-10-13 17:16:38 -0400

    On get $ send cmd string

--- main.ts
+++ main.ts
@@ -1,6 +1,9 @@
-serial.onDataReceived(serial.delimiters(Delimiters.Dollar), function on_data_received() {
-    
+serial.onDataReceived(serial.delimiters(Delimiters.Dollar), function () {
+    serial.writeLine("" + convertToText(button) + "," + convertToText(pitch) + "," + convertToText(roll))
 })
-basic.forever(function on_forever() {
-    
-})
+let roll = 0
+let pitch = 0
+let button = 0
+button = 0
+pitch = 0
+roll = 0
